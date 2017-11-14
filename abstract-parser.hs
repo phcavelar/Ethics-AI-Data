cf439f414969fb56cb96e19a48be776212328d3f
Marcelo Prates <marceloprates@bitbucket.org>
2017-11-14 19:13:04 -0200

    Added abstract parser

--- abstract-parser.hs
+++ abstract-parser.hs
@@ -7,6 +7,7 @@ import Data.String.Utils
 import Data.Char
 import Text.Printf
 import System.FilePath
+import Data.Ord
 
 find_keywords :: String -> [String] -> [Int]
 find_keywords text keywords = map (\x -> length $ findIndices ((==x)) $ map (map toLower) $ splitWs text) keywords
@@ -14,11 +15,15 @@ find_keywords text keywords = map (\x -> length $ findIndices ((==x)) $ map (map
 accumulate :: Num a => [[a]] -> [a]
 accumulate = foldr (zipWith (+)) (repeat 0)
 
+findYear x = head $ (filter (all isDigit) x) ++ ["0"]
+findAbstract = maximumBy (comparing length)
+
 main = do
 
-	keywords 					<- lines <$> readFile "dblp/keywords.txt"
-	entries  					<- map (\x -> let y = splitOn "#" x in (replace ";" "" $ y!!0,y!!3,y!!9)) <$> lines <$> readFile "nips.txt"
-	let entries_keywords		= filter (\x -> any (>0) $ (++) (snd3 x) (thd3 x) ) $ map (\x -> (fst3 x, find_keywords (snd3 x) keywords, find_keywords (thd3 x) keywords )) entries
-	let entries_keywords_year 	= map (\x -> (fst3 $ head x, accumulate $ map (\y -> zipWith (+) (snd3 y) (thd3 y)) x)) $ groupBy (\a b -> (fst3 a) == (fst3 b)) $ sortBy (\a b -> compare (fst3 a) (fst3 b)) $ entries_keywords
-	
-	writeFile "stats/nips.dat" $ unlines $ map (\x -> printf "%s %s" (fst x) (intercalate " " $ map show $ snd x)) entries_keywords_year
+	keywords 					<- (sort . lines) <$> readFile "dblp/keywords.txt"
+	entries 					<- map (\x -> (replace ";" "" $ replace "\n" "" $ x!!0, findAbstract x)) <$> (chunksOf 10 . init . splitOn "#") <$> readFile "abstracts/aaai.txt"
+
+	let entries_keywords 			= map (\(year,abstract) -> (year, find_keywords abstract keywords)) entries
+	let entries_keywords_byyear 	= map (\x -> (fst $ head x, accumulate $ map snd x, (map (/(fromIntegral $ length x)) $ map fromIntegral $ accumulate $ map snd x)::[Double] ) ) $ groupBy (\a b -> (fst a) == (fst b)) $ sortBy (comparing fst) entries_keywords 
+
+	writeFile "stats/abstracts-aaai.dat" $ unlines $ map (\x -> printf "%s %d %0.3f" (fst3 x) (sum $ snd3 x) (sum $ thd3 x) ) entries_keywords_byyear
